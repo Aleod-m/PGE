b879803e904314b9861fb9323a89905716665a84
Derobert <adriendml99@gmail.com>
2020-10-28 00:20:56 +0000

    Created a ressource management system

---
+++ src/ressources/mod.rs
@@ -0,0 +1,64 @@
+pub mod png;
+
+use std::path::{Path, PathBuf};
+use std::fs;
+use std::io::{self, Read};
+use std::ffi;
+
+#[derive(Debug)]
+pub enum Error {
+    FailedToGetPath,
+    FileContainsNullByte,
+    IO(io::Error)
+}
+
+pub struct Ressources {
+    path : PathBuf,
+}
+
+impl Ressources {
+
+    pub fn from_rel_path(rel_path : &Path) -> Result<Ressources, Error> {
+        let exe_name = std::env::current_exe().map_err(|_| Error::FailedToGetPath)?;
+        let exe_path = exe_name.parent().ok_or(Error::FailedToGetPath)?;
+        Ok(Self {
+            path : exe_path.join(rel_path),
+        })
+    }
+
+    pub fn name_to_path(root_dir : &Path, location : &str) -> PathBuf {
+        let mut path: PathBuf = root_dir.into();
+
+        for part in location.split("/") {
+            path = path.join(part);
+        }
+    
+        path
+    }
+
+    pub fn load_cstring(&self, name : &str) -> Result<ffi::CString, Error> {
+        let mut file = fs::File::open(self.path.join(name))?;
+
+        let mut content: Vec<u8> = Vec::with_capacity(
+            file.metadata()?.len() as usize + 1
+        );
+        file.read_to_end(&mut content)?;
+
+        if content.iter().find(|i| **i == 0).is_some() {
+            return Err(Error::FileContainsNullByte);
+        }
+
+        Ok(
+            unsafe {
+                ffi::CString::from_vec_unchecked(content)
+            }
+        )
+    }
+}
+
+
+impl From<io::Error> for Error {
+    fn from(other: io::Error) -> Self {
+        Error::IO(other)
+    }
+}
