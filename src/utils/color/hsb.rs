c4b3fabd38b095ddac8e5aaae225e0b7bafcfef6
Derobert <adriendml99@gmail.com>
2020-11-19 09:45:18 +0000

    modified match

--- src/utils/color/hsb.rs
+++ src/utils/color/hsb.rs
@@ -27,12 +27,16 @@ impl From<RgbColor> for HsbColor  {
         let max = r.max(g.max(b));
         let min = r.min(g.min(b));
         let range = max - min;
-        let hue = match (range, max == r, max == g, max == b) {
-            (0f32,_,_,_)    => 0f32,
-            (_,true,_,_) => ((g - b) / range % 6f32 ) * 60f32,
-            (_,_,true,_) => ((b - r) / range + 2f32) * 60f32,
-            (_,_,_,true) => ((r - g) / range + 4f32) * 60f32,
-            (_, false, false, false) => 0f32,
+        let hue = if range == 0f32 {
+            0f32
+        }
+        else {
+            match (max == r, max == g, max == b) {
+                (true,_,_) => ((g - b) / range % 6f32 ) * 60f32,
+                (_,true,_) => ((b - r) / range + 2f32) * 60f32,
+                (_,_,true) => ((r - g) / range + 4f32) * 60f32,
+                (false, false, false) => 0f32,
+            }
         };
 
         let brightness = max;
