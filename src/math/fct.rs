4bfc2ae926ba1e405df500601db5a653e491af01
Derobert <adriendml99@gmail.com>
2021-01-11 12:34:28 +0000

    Added a fast inverse square root function

--- src/math/fct.rs
+++ src/math/fct.rs
@@ -14,3 +14,18 @@ pub fn soft_min(a : f32, b : f32, k : f32) -> f32 {
 pub fn map(val:f32, ai:f32, bi:f32, af:f32, bf:f32) -> f32 {
     af + (val - ai) / (bi-ai) * (bf - af)
 }
+
+
+/// Fast inv sqrt algorithm from the quakeIII engine
+/// compute the value (1/nb)^(-0.5)
+pub fn fast_isqrt(nb : f32) -> f32 {
+    let x2 = nb * 0.5f32;
+    let mut y = nb;
+    let mut i = y.to_bits();
+    i =  0x5f3759dfu32 - (i>>1);
+    y = f32::from_bits(i);
+    y = y * (1.5f32 - (x2 * y * y));
+    y = y * (1.5f32 - (x2 * y * y));
+    return y;
+}
+
